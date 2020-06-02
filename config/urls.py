c6c2d22d4ef10a14bb41882df2c17155018f787c
erp_dev <erp_dev@invento.com.bd>
2020-06-02 20:01:47 +0600

    Initial commit.

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, reverse_lazy, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# Media and Static files for development
+
 # Media and Static files for development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
